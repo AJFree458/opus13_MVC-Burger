fd97c7219be4dd463ff4e6cebe0ed1a282ba137b
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-04-17 22:08:51 -0400

    Make seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burgers, devoured)
+VALUES
+    ("Cheeseburger", TRUE),
+    ("Double Cheeseburger", TRUE),
+    ("Bacon Cheeseburger", TRUE),
+    ("Burger", false),
+    ("Buffalo Burger", false),
+    ("Veggie Burger", false);
