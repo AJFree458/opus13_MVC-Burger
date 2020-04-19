40117485d3352186c94837d5c4e4459f2a73aa13
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-04-19 15:11:09 -0400

    Fixed a boolean default and added primary key

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN DEFAULT false NOT NULL,
+    PRIMARY KEY (id)
 );
